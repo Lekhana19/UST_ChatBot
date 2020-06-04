eed763f349ba28f32e29df6c23fbaacfd448d2be
Pratinav Verma <Gameboy98@users.noreply.github.com>
2020-06-04 20:24:00 +0530

    hr

---
+++ Train.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  4 17:57:18 2020
+
+@author: PRATINAV
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
